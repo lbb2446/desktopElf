454894174ab38ad8204c0356180df47df22a593c
lbb <814797346@qq.com>
2020-11-28 14:22:55 +0800

    feat:托盘功能

--- src/main/index.js
+++ src/main/index.js
@@ -21,7 +21,7 @@ function createWindow () {
    * Initial window options
    */
   mainWindow = new BrowserWindow({
-    height: 426,
+    height: 450,
     useContentSize: true,
     width: 204,
     frame: false, // 无边框
@@ -72,31 +72,31 @@ app.on('ready', async () => {
   // if (isDevelopment && !process.env.IS_TEST) {
   // }
   // 设置托盘
-  // let iconPath = path.join(__dirname,"../renderer/assets/logo.png")
-  // const tray = new Tray(iconPath)
-  // // 设置托盘菜单
-  // const trayContextMenu = Menu.buildFromTemplate([
-  //   {
-  //     label: '打开',
-  //     click: () => {
-  //       mainWindow.show()
-  //     }
-  //   }, {
-  //     label: '退出',
-  //     click: () => {
-  //       app.quit()
-  //     }
-  //   }
-  // ])
-  // tray.setToolTip('myApp')
-  // tray.on('click', () => {
-  //   mainWindow.show()
-  // })
-  // tray.on('right-click', () => {
-  //   tray.popUpContextMenu(trayContextMenu)
-  // })
-  // 创建渲染窗口
-  createWindow()
+  await createWindow()
+  let iconPath = path.join(__dirname,"../renderer/assets/logo.png")
+  const tray = new Tray(iconPath)
+  // 设置托盘菜单
+  const trayContextMenu = Menu.buildFromTemplate([
+    {
+      label: '打开',
+      click: () => {
+        mainWindow.show()
+      }
+    }, {
+      label: '退出',
+      click: () => {
+        app.quit()
+      }
+    }
+  ])
+  tray.setToolTip('myApp')
+  tray.on('click', () => {
+    mainWindow.show()
+  })
+  tray.on('right-click', () => {
+    tray.popUpContextMenu(trayContextMenu)
+  })
+  
 })
 
 app.on('window-all-closed', () => {
