2b2f4549725a3131ac98a43e21289c1e083b032d
lbb <814797346@qq.com>
2021-02-08 14:03:13 +0800

    新增输入框

--- src/renderer/components/mood.vue
+++ src/renderer/components/mood.vue
@@ -1,30 +1,38 @@
 <template>
     <div class="info">
         {{msg}}
+        {{todolist}}
+        <input type="text" v-model="task" @keyup.enter="submit">
     </div>
 </template>
 
 <script>
 import { mapState, mapActions,mapGetters } from "vuex";
-import {store,mutations} from './minimood'
+import {store,mutations,log} from './minimood'
+
 //负责情绪调度以及语言模块
     export default {
         name: '',
         computed:{
             msg(){
-                console.log(store)
                 return store.speak
             }
         },
         data() {
             return {
-                
+                todolist:[],
+                task:""
             }
+            
         },mounted(){
-            //启动的时候默认
-            // setInterval(()=>{
-            //     mutations.add({b:1234})
-            // },1000)
+        },
+        methods:{
+            submit(){
+                log("好的，我已经收到了哦")
+                this.todolist.push(this.task)
+                console.log(this.todolist)
+                this.task=""
+            }
         }
     }
 </script>
