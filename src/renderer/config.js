42a506db9f57bf2bfc74f6c7e062eeb19c090528
lbb <814797346@qq.com>
2021-04-26 17:33:19 +0800

    修复部分无用代码

--- src/renderer/config.js
+++ src/renderer/config.js
@@ -1,3 +1,2 @@
-let shell = {
-
-}
+export const RSS_LIst = [""]
+export const RSS_RELOAD_TIME = 3600
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,31 +5,22 @@ import VueContextMenu from '@xunlei/vue-context-menu'
 import App from './App'
 import router from './router'
 import store from './store'
-// import 'ant-design-vue/dist/antd.css'
-// import Antd from 'ant-design-vue'
-// import ref from 'vue-ref'
-
 import iView from 'iview'
 import 'iview/dist/styles/iview.css'
 import db from './datastore.js'
-// Vue.use(Antd)
-// Vue.use(ref, { name: 'ant-ref' })
 Vue.prototype.$db = db
 Vue.use(iView)
 Vue.use(VueContextMenu)
-// import { DatePicker } from 'ant-design-vue'
-// import 'ant-design-vue/dist/antd.css'
-// Vue.use(DatePicker)
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  components: {
-    App
-  },
-  router,
-  store,
-  template: '<App/>'
+    components: {
+        App
+    },
+    router,
+    store,
+    template: '<App/>'
 }).$mount('#app')
